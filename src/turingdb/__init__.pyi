004e606f32f3b23a51af77e5f7aa6e310fcf38b4
Luc Labarrière <luc.labarriere@gmail.com>
2025-10-31 11:29:37 +0100

    Fixed stub issues

--- src/turingdb/__init__.pyi
+++ src/turingdb/__init__.pyi
@@ -1,4 +1,4 @@
 from .turingdb import TuringDB as TuringDB, TuringDBException as TuringDBException
 from .turingsh import main as turingsh
 
-__all__ = ["TuringDB", "TuringDBException", "turingsh"]
+__all__ = ['TuringDB', 'TuringDBException', 'turingsh']
--- src/turingdb/turingsh/__init__.pyi
+++ src/turingdb/turingsh/__init__.pyi
@@ -1,3 +1,3 @@
 from .turingsh import main as main
 
-__all__ = ["main"]
+__all__ = ['main']
