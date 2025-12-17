003ce5855c5ef8d92ed546a9b8985b969ffa2759
Luc Labarrière <luc.labarriere@gmail.com>
2025-12-17 14:28:22 +0100

    Updated the S3 commands to match v2 syntax

--- examples/imports.py
+++ examples/imports.py
@@ -1,13 +1,13 @@
 from turingdb import TuringDB
 
 if __name__ == "__main__":
-    client = TuringDB(host="http://localhost:1234")
+    client = TuringDB(host="http://localhost:6666")
 
     print("- Connecting to S3")
-    client.s3_connect("mybucket", region="eu-west-2", use_scratch=True)
+    client.s3_connect("turing-internal", region="eu-west-2", use_scratch=True)
 
     print("- Uploading ./mygraph.gml")
     client.transfer(src="~/mygraph.gml", dst="turingdb://mygraph.gml")
 
     print("- Loading mygraph.gml")
-    client.query('IMPORT GRAPH mygraph FROM "mygraph.gml"')
+    client.query('LOAD GML "mygraph.gml" AS mygraph')
