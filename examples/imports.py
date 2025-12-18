a8c22a890d020742e4a10e2e987a3f790cbfaee5
Luc Labarrière <luc.labarriere@gmail.com>
2025-12-18 19:21:46 +0100

    Fixed the imports.py example

--- examples/imports.py
+++ examples/imports.py
@@ -1,10 +1,10 @@
 from turingdb import TuringDB
 
 if __name__ == "__main__":
-    client = TuringDB(host="http://localhost:6666")
+    client = TuringDB(host="http://localhost:1234")
 
     print("- Connecting to S3")
-    client.s3_connect("turing-internal", region="eu-west-2", use_scratch=True)
+    client.s3_connect("mybucket", region="eu-west-2", use_scratch=True)
 
     print("- Uploading ./mygraph.gml")
     client.transfer(src="~/mygraph.gml", dst="turingdb://mygraph.gml")
